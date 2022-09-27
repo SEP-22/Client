c0518e59ae96b870fa2ff71ae06c79e56627152b
GimhanRanasinghe <gimhanravi@gmail.com>
2022-09-28 01:28:26 +0530

    login connected

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { React, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { styled } from "@mui/material/styles";
 import Navbar from "../../components/Navbar/NabvarUser";
 import Footer from "../../components/Footer/Footer";
@@ -9,8 +9,30 @@ import NavbarLanding from "../../components/Navbar/NavbarLanding";
 import loginImg from "../../assets/images/loginImg.png";
 import TextField from "@mui/material/TextField";
 import "./loginPage.css";
+import { signIn } from "../../utils/api/user";
 
 export default function LandingPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isMatch, setIsMatch] = useState(true);
+
+  const nav = useNavigate();
+
+  const login = async (event) => {
+    event.preventDefault();
+    if (username != "" && password != "") {
+      const res = await signIn({ username, password });
+      if (res.data.message=="success") {
+        nav("/");
+      }else if (res.data.message == "invalid email or password") {
+        setIsMatch(false);
+        console.log(res.data.message);
+      }
+    } else {
+      alert("Invalid inputs");
+    }
+  };
+
   return (
     <>
       <div className="loginContainer">
@@ -25,20 +47,29 @@ export default function LandingPage() {
               variant="outlined"
               required
               fullWidth
+              onChange={() => {
+                setUsername(event.target.value);
+              }}
             />
             <TextField
               style={{ marginBottom: "3vh" }}
               id="outlined-basic"
               label="Password"
               variant="outlined"
+              type="password"
               required
               fullWidth
+              onChange={() => {
+                setPassword(event.target.value);
+              }}
             />
+            {!isMatch ? <p>Invalid email or password</p>:<p></p>}
             <Button
               className="formItem"
               style={{ backgroundColor: "#4CAF50", marginBottom: "3vh" }}
               variant="contained"
               fullWidth
+              onClick={login}
             >
               Login
             </Button>
