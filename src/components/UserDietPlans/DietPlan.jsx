c990e33b49d6743f7c4d4f2d70217245f94e0806
Jithmi004 <79504341+Jithmi004@users.noreply.github.com>
2022-09-30 01:23:58 +0530

    dietplan UI done

--- src/components/UserDietPlans/DietPlan.jsx
+++ src/components/UserDietPlans/DietPlan.jsx
@@ -4,6 +4,8 @@ import { styled } from "@mui/material/styles";
 import FoodCardAdmin from "../FoodCard/FoodCardAdmin";
 import Meal from "../Meal/Meal"
 import "./UserDietPlans.css"
+import { width } from "@mui/system";
+import Chip from '@mui/material/Chip';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: "transparent",
@@ -15,6 +17,43 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function FoodList() {
   return (
+    <>
+    <Box
+        sx={{
+            m: 2,
+            alignItems: "center",
+            display: "flex",
+            justifyContent: "center",
+            margin: "1px",
+          }}
+      >
+        <Paper 
+        sx={{ mt: 4, mb: 4, p: 4, 
+          alignItems: "center" , 
+          display: "flex",
+          flexDirection: "column",
+          width:"50%"}}
+        >
+        <Grid item xs={12}>
+          <Button variant="contained" sx={{mb:4}}>
+            Create a Diet Plan
+          </Button>
+        </Grid>
+          
+          <Typography
+          sx={{ fontWeight: "light" }}
+          variant="h6"
+          component="h4"
+          color="dark"
+          align="center"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae vehicula dui. Etiam consectetur porta tellus, vel porta leo scelerisque semper. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean in dapibus metus, in ultrices libero. Nulla vel placerat lectus, a commodo elit.
+          </Typography>
+          <br></br>
+          <br></br>
+        </Paper>
+          
+      </Box>
     <Box
       sx={{
         m: 2,
@@ -49,7 +88,8 @@ export default function FoodList() {
             >
               Diet Plan 1
             </Typography>
-            <Button>Active</Button>
+            {/* <Button>Active</Button> */}
+            <Chip label="Active" color="primary" />
           </Item>
           
         </Grid>
@@ -62,5 +102,6 @@ export default function FoodList() {
       ))} */}
       </Grid>
     </Box>
+    </>
   );
 }
