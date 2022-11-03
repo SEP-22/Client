507782e1c666c9a78ee0b33f86fbf2c22d488015
Jithmi004 <79504341+Jithmi004@users.noreply.github.com>
2022-11-03 17:57:18 +0530

    some changes

--- src/components/User/EditName.jsx
+++ src/components/User/EditName.jsx
@@ -1,11 +1,44 @@
 import { Button, FormControl, InputLabel, OutlinedInput, Paper, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
-import { useNavigate } from "react-router-dom";
+import { redirect, useNavigate } from "react-router-dom";
+import { getASingleUser } from '../../utils/api/user';
 
 const EditName = () => {
 
   const navigate = useNavigate();
+
+  const[profileDet , setProfileDet] = React.useState({});
+  const _id = "6335d3657e7aaea82d5e3650"
+  const[newName,setNewName] = React.useState('')
+  //const[newName,setNewName] = React.useState(profileDet.name)
+
+  React.useEffect(() =>{
+    const getData = async() => {
+      const res = await getASingleUser(_id);
+      if(res.status == 200) {
+        const data = res.data;
+        setProfileDet(data)
+      }else{
+        console.log("error")
+      }
+    };
+    getData();
+  },[]);
+
+  function handleChange(event) {
+    setNewName(event.target.value)
+    //console.log(event.target.value)
+  }
+
+  function changeName(){
+    console.log(newName)
+    if(newName.trim() != "") {
+      //update the name in db
+      navigate("/eatsmart/profile");
+    }
+    //notify that name cannot be empty
+  }
 
   return (
     <>
@@ -40,7 +73,11 @@ const EditName = () => {
                     name='name'
                     label="Name"
                     required
-                    value={"Jonathan Herondale"}
+                    //value={"Jonathan Herondale"}
+                    //key={`${Math.floor((Math.random() * 1000))}-min`}
+                    defaultValue={profileDet.name}
+                    //value={profileDet.name}
+                    onChange={handleChange}
                     />
                 </FormControl>
             </Box>
@@ -57,11 +94,11 @@ const EditName = () => {
               <Button
                 variant="outlined"
                 color="secondary"
-                onClick={() => navigate(-1)}
+                onClick={() => navigate("/eatsmart/profile")}
               >
                 Back
               </Button>
-              <Button variant="contained" type="submit" >
+              <Button variant="contained" type="submit" onClick={changeName}>
                 Save
               </Button>
             </Box>
