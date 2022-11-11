5a6389eaf130ff9eb6dc627764c399b0b58cb2bf
Jithmi004 <79504341+Jithmi004@users.noreply.github.com>
2022-11-11 23:27:21 +0530

    activate diet plan dialog box

--- src/components/UserDietPlans/singleDiet.jsx
+++ src/components/UserDietPlans/singleDiet.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Box, Paper, Typography, Button } from "@mui/material";
+import { Grid, Box, Paper, Typography, Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import FoodCardAdmin from "../FoodCard/FoodCardAdmin";
 import Meal from "../Meal/Meal"
@@ -18,6 +18,15 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 const SingleDietPlan = (props) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const completeDetails = props.completeDet.dietIDs;
   const status = props.active;
    //console.log("here i log",status);
@@ -77,7 +86,28 @@ const SingleDietPlan = (props) => {
           <Button variant="outlined" color="secondary" sx={{m:2}}>View</Button>
           {!status && (
             <>
-            <Button variant="contained" color="primary">Activate</Button>
+            <Button variant="contained" color="primary" onClick={handleClickOpen}>Activate</Button>
+            <Dialog 
+              open = {open}
+              onClose = {handleClose}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+            >
+              <DialogTitle id = "alert-dialog-title">
+                {"Change Active Diet Plan?"}
+              </DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                  Are you sure you want to set this as your active diet plan?
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose} color="primary">No</Button>
+                <Button onClick={handleClose} color="secondary" autoFocus>
+                  Yes
+                </Button>
+              </DialogActions>
+            </Dialog>
             </>
           )}
           {/* <Button variant="contained" color="primary">Activate</Button> */}
