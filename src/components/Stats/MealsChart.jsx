a8ee3988dca025d10ef208a6181f10eb18cced1c
KaveeshaPiumini <piumini95kaveesha@gmail.com>
2022-11-12 01:13:47 +0530

    Create MealsChart.jsx

---
+++ src/components/Stats/MealsChart.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Chart } from "react-google-charts";
+export const data = [
+  ["Meals", "Calory Percentage"],
+  ["Breakfast", 30],
+  ["Lunch", 40],
+  ["Dinner", 30],
+];
+export const options = {
+  chartArea:{left:20,top:40,width:'100%',height:'75%'},
+  fontName:"Roboto",
+  colors:['#ba68c8','#ef5350', "#4fc3f7", "#9ccc65","#ffc107" ,],
+};
+
+const CategoryChart = () => {
+  return (
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"300px"}
+    />
+  );
+};
+
+export default CategoryChart;
