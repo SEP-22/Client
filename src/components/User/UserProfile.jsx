cb3e37aa4bb55b00a339cd01207cb1f2c6fade0d
Jithmi004 <79504341+Jithmi004@users.noreply.github.com>
2022-11-18 03:20:36 +0530

    displaying error messages in edit profile done

--- src/components/User/UserProfile.jsx
+++ src/components/User/UserProfile.jsx
@@ -15,8 +15,7 @@ import { getASingleUser } from '../../utils/api/user';
 
 const UserProfile = () => {
 
-  const[profileDet , setProfileDet] = React.useState({});
-  //const _id = "6335d3657e7aaea82d5e3650"
+  const[profileDet , setProfileDet] = React.useState({name:"",email:"",phone:"",password:""});
   const _id = JSON.parse(localStorage.getItem("user")).id;
 
   React.useEffect(() =>{
@@ -32,19 +31,10 @@ const UserProfile = () => {
     getData();
   },[]);
 
-   const str = (Detail.Type.padEnd(20,' ')+profileDet.name).padEnd(60,' ');
-   const str1 = (Detail1.Type.padEnd(20,' ')+profileDet.email).padEnd(60,' ');
-   const str2 = (Detail2.Type.padEnd(20,' ')+profileDet.phone).padEnd(60,' ');
-  const str3 = (Detail3.Type.padEnd(20,' ')+profileDet.password).padEnd(60,' ');
+   const str = ("Name".padEnd(20,' ')+profileDet.name).padEnd(60,' ');
+   const str1 = ("Email".padEnd(20,' ')+profileDet.email).padEnd(60,' ');
+   const str2 = ("Phone".padEnd(20,' ')+profileDet.phone).padEnd(60,' ');
   
-
-
-  //const str = (Detail.Type.padEnd(20,' ')+Detail.Value).padEnd(60,' ');
-  //const str1 = (Detail1.Type.padEnd(20,' ')+Detail1.Value).padEnd(60,' ');
-  //const str2 = (Detail2.Type.padEnd(20,' ')+Detail2.Value).padEnd(60,' ');
-   //const str3 = (Detail3.Type.padEnd(20,' ')+Detail3.Value).padEnd(60,' ');
-  //const str2 = str1.padEnd(15,' ');
-
   const navigate = useNavigate();
 
   return (
@@ -71,10 +61,6 @@ const UserProfile = () => {
           src="https://i.pinimg.com/564x/a6/58/32/a65832155622ac173337874f02b218fb--people-icon-avatar.jpg"
           sx={{ width: 200, height: 200, margin:"30px"}}>
         </Avatar>
-        {/* <ProfileDetail Detail={Detail}></ProfileDetail>
-        <ProfileDetail Detail={Detail1}></ProfileDetail>
-        <ProfileDetail Detail={Detail2}></ProfileDetail>
-        <ProfileDetail Detail={Detail3}></ProfileDetail> */}
         <div style={{
           display: 'flex',
           alignItems: 'center',
@@ -86,8 +72,6 @@ const UserProfile = () => {
             </Typography>
           </CardContent>
           <CardActions>
-            {/* <Button variant="contained" endIcon={<EditIcon />}>
-          </Button> */}
             <IconButton size='small' color='primary' component={Link} to="editname">
               <EditIcon fontSize='small'/>
             </IconButton>
@@ -122,32 +106,11 @@ const UserProfile = () => {
             </Typography>
           </CardContent>
           <CardActions>
-            {/* <Button variant="contained" endIcon={<EditIcon />}>
-          </Button> */}
             <IconButton size='small' color='primary' component={Link} to="editphone"> 
               <EditIcon fontSize='small'/>
             </IconButton>
           </CardActions>
         </div>
-        {/* <div style={{ */}
-          {/* display: 'flex', */}
-          {/* alignItems: 'space-between', */}
-          {/* borderBottom: '1px solid grey', */}
-          {/* marginBottom: '50px' */}
-        {/* }}> */}
-          {/* <CardContent> */}
-            {/* <Typography variant="body2" color="text.Secondary"> */}
-            {/* <pre>{str3}</pre> */}
-            {/* </Typography> */}
-          {/* </CardContent> */}
-          {/* <CardActions> */}
-            {/* <Button variant="contained" endIcon={<EditIcon />}>
-          </Button> */}
-            {/* <IconButton size='small' color='primary'> */}
-              {/* <EditIcon fontSize='small'/> */}
-            {/* </IconButton> */}
-          {/* </CardActions> */}
-        {/* </div> */}
         <br />
         <br />
         <div style={{
@@ -157,9 +120,6 @@ const UserProfile = () => {
           <CardActions>
             <Button variant="contained" component={Link} to="editpassword" endIcon={<EditIcon />}> Edit Password
           </Button>
-            {/* <IconButton size='small' color='primary' component={Link} to="editphone" variant = 'contained'> 
-              Change Password<EditIcon fontSize='small'/>
-            </IconButton> */}
           </CardActions>
         </div>
         <br />
@@ -171,27 +131,3 @@ const UserProfile = () => {
 }
 
 export default UserProfile
-
-const Person = {
-    Name: "Jithmi",
-    Email: "jithmi.nawoda01@gmail.com",
-    PhoneNumber: "0762878555",
-    Password: "########",
-    Image: "https://img.rolandberger.com/persons/RB_Peter_Magunia_square_person2_none.png",
-}
-const Detail = {
-  Type: "Name",
-  Value: "Jonathan Herondale",
-}
-const Detail1 = {
-  Type: "Email",
-  Value: "jithmi.nawoda01@gmail.com",
-}
-const Detail2 = {
-  Type: "Phone Number",
-  Value: "0762878555",
-}
-const Detail3 = {
-  Type: "Password",
-  Value: "########",
-}
