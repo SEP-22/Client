5e4d00f6b02c5b5a94110781e257bba2cb8e941f
Jithmi004 <79504341+Jithmi004@users.noreply.github.com>
2022-11-18 04:33:03 +0530

    profile page responsive done

--- src/components/User/UserProfile.jsx
+++ src/components/User/UserProfile.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-//import "../../pages/Profile/profilePage.css"
+import "../../pages/Profile/profilePage.css"
 import Card from '@mui/material/Card';
-import { Avatar, CardContent, Typography, CardActions, Button } from '@mui/material';
-import { borderBottom, Box, margin, width } from '@mui/system';
+import { Avatar, CardContent, Typography, CardActions, Button, ListItemText } from '@mui/material';
+import { borderBottom, Box, margin, minWidth, width } from '@mui/system';
 import ProfileDetail from './ProfileDetail';
 import SendIcon from '@mui/icons-material/Send';
 import EditIcon from '@mui/icons-material/Edit';
@@ -11,7 +11,12 @@ import Modal from '@mui/material/Modal';
 import { Link, useNavigate } from "react-router-dom";
 import { getASingleUser } from '../../utils/api/user';
 //import SmileIcon from "@mui/icons/Mood";
-
+import Grid from '@mui/material/Grid';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import List from '@mui/material/List';
+import { FolderSpecialOutlined } from '@mui/icons-material';
+import Paper from '@mui/material/Paper';
 
 const UserProfile = () => {
 
@@ -31,42 +36,104 @@ const UserProfile = () => {
     getData();
   },[]);
 
-   const str = ("Name".padEnd(20,' ')+profileDet.name).padEnd(60,' ');
-   const str1 = ("Email".padEnd(20,' ')+profileDet.email).padEnd(60,' ');
-   const str2 = ("Phone".padEnd(20,' ')+profileDet.phone).padEnd(60,' ');
+   const str = ("Name".padEnd(20,' ')+profileDet.name);
+   const str1 = ("Email".padEnd(20,' ')+profileDet.email);
+   const str2 = ("Phone".padEnd(20,' ')+profileDet.phone);
   
   const navigate = useNavigate();
 
   return (
     <Box
-      sx={{
-        m: 2,
-        alignItems: "center",
-        display: "flex",
-        justifyContent: "center",
-      }}
-    >
-      <Card 
-        sx={{ 
-          width: "60%",
+        sx={{
+          m: 2,
           alignItems: "center",
           display: "flex",
-          flexDirection: "column",
           justifyContent: "center",
-          minWidth: 700,
+          margin: "1px",
         }}
-        //container spacing={{ xs: 4, md: 2 }}
       >
-        <Avatar alt="Profile Picture"
+      
+      <Paper
+          sx={{
+            mt: 4,
+            mb: 4,
+            p: 4,
+            alignItems: "center",
+            display: "flex",
+            flexDirection: "column",
+            width: "50%",
+          }}
+        >
+          <br></br>
+          <Avatar alt="Profile Picture"
           src="https://i.pinimg.com/564x/a6/58/32/a65832155622ac173337874f02b218fb--people-icon-avatar.jpg"
-          sx={{ width: 200, height: 200, margin:"30px"}}>
+          sx={{ width: 150, height: 150, margin:"30px"}}>
         </Avatar>
-        <div style={{
+        <div className='data' style={{
+          display: 'flex',
+          alignItems: 'center',
+          borderBottom: '1px solid grey'}}>
+        <ListItem alignItems='flex-start'
+            secondaryAction={<IconButton edge="end" aria-label='edit' size='small' color='primary' component={Link} to="editname"><EditIcon></EditIcon></IconButton>}>
+            <ListItemText primary = "Name" secondary = {profileDet.name} /> 
+          </ListItem> 
+        </div>
+        <div className='data' style={{
+          display: 'flex',
+          alignItems: 'center',
+          borderBottom: '1px solid grey'}}>
+        <ListItem
+            secondaryAction={<IconButton edge="end" aria-label='edit' size='small' color='primary' component={Link} to="editemail"><EditIcon></EditIcon></IconButton>}>
+            <ListItemText primary = "Email" secondary = {profileDet.email} /> 
+          </ListItem>
+        </div>
+        <div className='data' style={{
+          display: 'flex',
+          alignItems: 'center',
+          borderBottom: '1px solid grey'}}>
+        <ListItem
+            secondaryAction={<IconButton edge="end" aria-label='edit' size='small' color='primary' component={Link} to="editphone"><EditIcon></EditIcon></IconButton>}>
+            <ListItemText primary = "Phone" secondary = {profileDet.phone} /> 
+          </ListItem>
+        </div>
+        <br></br><br />
+          <Button variant="contained" component={Link} to="editpassword" endIcon={<EditIcon />}> Edit Password
+          </Button>
+          <br></br>
+        </Paper>
+      
+        
+       
+        {/* <Grid item xs={12} md={6}>
+          <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+            Avatar with text and icon
+          </Typography>
+            <List dense={dense}>
+                <ListItem
+                  secondaryAction={
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+                  }
+                >
+                  <ListItemAvatar>
+                    <Avatar>
+                      <FolderIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Single-line item"
+                    secondary={secondary ? 'Secondary text' : null}
+                  />
+                </ListItem>,
+            </List>
+        </Grid> */}
+        {/* <div style={{
           display: 'flex',
           alignItems: 'center',
           borderBottom: '1px solid grey'
-        }}>
-          <CardContent>
+        }}> */}
+          {/* <CardContent>
             <Typography variant="body2" color="text.Secondary">
             <pre>{str}</pre>
             </Typography>
@@ -75,9 +142,9 @@ const UserProfile = () => {
             <IconButton size='small' color='primary' component={Link} to="editname">
               <EditIcon fontSize='small'/>
             </IconButton>
-          </CardActions>
-        </div>
-        <div style={{
+          </CardActions> */}
+        {/* </div> */}
+        {/* <div style={{
           display: 'flex',
           alignItems: 'center',
           borderBottom: '1px solid grey'
@@ -88,14 +155,12 @@ const UserProfile = () => {
             </Typography>
           </CardContent>
           <CardActions>
-            {/* <Button variant="contained" endIcon={<EditIcon />}>
-          </Button> */}
             <IconButton size='small' color='primary' component={Link} to="editemail">
               <EditIcon fontSize='small'/>
             </IconButton>
           </CardActions>
-        </div>
-        <div style={{
+        </div> */}
+        {/* <div style={{
           display: 'flex',
           alignItems: 'center',
           borderBottom: '1px solid grey'
@@ -110,7 +175,7 @@ const UserProfile = () => {
               <EditIcon fontSize='small'/>
             </IconButton>
           </CardActions>
-        </div>
+        </div> */}
         <br />
         <br />
         <div style={{
@@ -118,13 +183,12 @@ const UserProfile = () => {
           alignItems: 'center',
         }}>
           <CardActions>
-            <Button variant="contained" component={Link} to="editpassword" endIcon={<EditIcon />}> Edit Password
-          </Button>
+            
           </CardActions>
         </div>
         <br />
         <br />
-      </Card>
+      
     </Box>
     
   )
